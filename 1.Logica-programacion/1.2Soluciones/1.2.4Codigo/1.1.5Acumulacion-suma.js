27b47e7e01f131c630eedb325f9850589039a26c
HazaelGuvm <marcosguzmanva@gmail.com>
2024-08-12 19:15:57 -0600

    nuevo3

--- 1.Logica-programacion/1.2Soluciones/1.2.4Codigo/1.1.5Acumulacion-suma.js
+++ 1.Logica-programacion/1.2Soluciones/1.2.4Codigo/1.1.5Acumulacion-suma.js
@@ -2,9 +2,22 @@ let numbers = [1, 4, 7, 9, 3, 3, 5, 90];
 
 let total = 0;
 
-for (let i = 0; i < numbers.length; i++){
+// for (let i = 0; i < numbers.length; i++){
 
+// debugger;
+//     if (i === 0){
+//         total = total + numbers[0];
+ 
+// }
+    
+//     if (i % 3 !== 0){
+//         total = total + numbers[i];
+// //console.log(numbers[i]);
+//     }
+// }
 
+let i = 0
+while (i < numbers.length){
     if (i === 0){
         total = total + numbers[0];
  
@@ -12,8 +25,8 @@ for (let i = 0; i < numbers.length; i++){
     
     if (i % 3 !== 0){
         total = total + numbers[i];
-console.log(numbers[i]);
+//console.log(numbers[i]);
     }
+    i++
 }
-
-console.log('total:', total);
+console.log('totallll:', total);
