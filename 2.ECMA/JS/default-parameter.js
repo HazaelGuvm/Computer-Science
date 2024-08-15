20a6fb69899f90f575f1d46d350abac04a0ef640
HazaelGuvm <marcosguzmanva@gmail.com>
2024-08-14 19:56:47 -0600

    nuevo7

---
+++ 2.ECMA/JS/default-parameter.js
@@ -0,0 +1,12 @@
+const sayHello = (name = 'sin nombre', age = 18) =>{
+
+    console.log(`El es ${name} y tiene ${age}`)
+
+}
+
+const studentName = 'Juanito'
+sayHello(studentName , 20)
+
+
+let senseiName = 'Jose'
+sayHello(senseiName , 30)
