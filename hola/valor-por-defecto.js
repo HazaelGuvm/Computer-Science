6cc95a623a43d021863a1c01f1a4ea4b1f89dd64
HazaelGuvm <marcosguzmanva@gmail.com>
2024-08-14 21:27:15 -0600

    nuevo9

---
+++ hola/valor-por-defecto.js
@@ -0,0 +1,12 @@
+const harryPotter ={
+    species:'human', 
+    gender:'male',
+    phone :'1234233'
+}
+const phone = harryPotter.phone|| ''
+const gender = harryPotter.gender|| ''
+
+
+console.log(phone)
+console.log(gender)
+
