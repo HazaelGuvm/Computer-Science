bd498f6cd28ac475acc9e511b16177eab3630373
HazaelGuvm <marcosguzmanva@gmail.com>
2024-08-14 19:37:58 -0600

    nuevo6

--- 2.ECMA/JS/fors-in-of.js
+++ 2.ECMA/JS/fors-in-of.js
@@ -20,6 +20,42 @@ const fruits = [
 //mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock"
 
 for(let i=0; i<fruits.length; i++){
-    console.log(fruits[i].name)
+    const fruit = fruits[i]
+    //console.log(fruit.name fruit.price fruit.isStock)
+
+    //operador ternario
+    //condicion ? resultadocondiciontrue : resultadocondicionfalse
+    const finalText = fruit.isStock? 'esta en stock':'no esta en stock'
+    // let finalText = ''
+    // if(fruit.isStock){
+    //     finalText = 'esta en stock'
+    // }else{
+    //     finalText ='no esta en stock'
+    // }
+console.log(`La ${fruit.name} cuesta ${fruit.price} y ${finalText}`)
 }
 
+//ES6 FOR OF es para iterar arreglos
+
+// for(const fruit of fruits){
+//     const fruit = fruits[i]    
+//     const finalText = fruit.isStock? 'esta en stock':'no esta en stock'
+//     console.log(`La ${fruit.name} cuesta ${fruit.price} y ${finalText}`)
+
+// ES6: FOR IN
+// Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock y lo venden en TIENDA1, TIENDA2, ... TIENDAN"
+for (let fruit of fruits) {
+    // Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock y lo venden en TIENDA1, TIENDA2, ... TIENDAN"
+    const message = fruit.isStock ? 'si' : 'no'
+    let storesMessage = '';
+    for (let prop in fruit) {
+        if (prop === 'stores') {
+            // console.log(fruit[prop]); // stores: ['buelmer', 'badiga eurrira', 'chadreui'] => 'buelmer', 'badiga eurrira', 'chadreui'
+            for (let store of fruit[prop]) {
+                storesMessage += `${store}`;
+                //  storesMessage += fruit[prop].length - 1 === i ? '.' : ',' como obtener la i?????
+            }
+        }
+    }
+    console.log(`La ${fruit.name} cuesta ${fruit.price} y ${message} esta en stock y lo venden en ${storesMessage}`);
+}
